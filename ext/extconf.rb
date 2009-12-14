6ecf8a0884a9c02124c70f271d88547d0be30b93
rogerdpack <rogerpack2005@gmail.com>
2009-12-14 16:05:31 -0700

    compile the outer dependency

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,4 +1,6 @@
 require 'mkmf'
+require 'rubygems'
+require 'sane'
 
 # Give it a name
 extension_name = 'mytest'
@@ -6,7 +8,14 @@ extension_name = 'mytest'
 # The destination
 dir_config(extension_name)
 
-$CFLAGS += " -I/C/go/include "
+$CFLAGS += " -I./local_installed "
 
-# Do the work
+dir = Dir.pwd
+Dir.chdir 'sparsehash-1.5.2' do
+  dir = dir + '/local_installed'
+  command = "sh configure --prefix=#{dir} && make && make install"
+  puts command
+  system command
+end
+
 create_makefile(extension_name)
