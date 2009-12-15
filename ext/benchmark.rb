c6b41006b4bcdd7e9c8ba5d3d09e614233b2c628
rogerdpack <rogerdpack@gmail.com>
2009-12-15 23:21:29 +0000

    add constant alias GoogleHash -> GoogleHashDense

--- ext/benchmark.rb
+++ ext/benchmark.rb
@@ -1,7 +1,7 @@
 require './google_hash'
 require 'benchmark'
 
-for name in [GoogleHashSparse, GoogleHashDense, Hash] do
+for name in [GoogleHashSparse, GoogleHashDense, Hash, GoogleHash] do
  subject = name.new
  puts name, Benchmark.realtime { 500000.times {|n| subject[n] = 4}}.to_s + "   (populate)"
  puts Benchmark.realtime { subject.each{|k, v| }}.to_s + " (each)", ''
