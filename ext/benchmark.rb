522c3bf09b7ad8a0b806ee0b3de8407d9f4f7900
rdp <rogerpack2005@gmail.com>
2009-12-15 15:25:45 -0700

    cleanup benchmark

---
+++ ext/benchmark.rb
@@ -0,0 +1,8 @@
+require 'google_hash'
+require 'benchmark'
+
+for name in [GoogleHashSparse, GoogleHashDense, Hash] do
+ subject = name.new
+ puts name,  Benchmark.realtime { 1000000.times {|n| subject[n] = 4}}
+ puts Benchmark.realtime { subject.each{ }}
+end
