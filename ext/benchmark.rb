5d62bf342c75b2987a921e6308890c8c4ffc6327
rdp <rogerpack2005@gmail.com>
2009-12-15 15:54:46 -0700

    possibly more real world-y test

--- ext/benchmark.rb
+++ ext/benchmark.rb
@@ -4,5 +4,6 @@ require 'benchmark'
 for name in [GoogleHashSparse, GoogleHashDense, Hash] do
  subject = name.new
  puts name, Benchmark.realtime { 500000.times {|n| subject[n] = 4}}.to_s + "   (populate)"
- puts Benchmark.realtime { subject.each{ }}.to_s + " (each)", ''
+ puts Benchmark.realtime { subject.each{|k, v| }}.to_s + " (each)", ''
+
 end
