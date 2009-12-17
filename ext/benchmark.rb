228384b45cd5f46da83115e9258f8819a1f38a7a
rdp <rogerpack2005@gmail.com>
2009-12-17 09:50:19 -0700

    update benchmark

--- ext/benchmark.rb
+++ ext/benchmark.rb
@@ -1,9 +1,15 @@
 require './google_hash'
 require 'benchmark'
 
+num = 500000
+puts num
+
 for name in [GoogleHashSparse, GoogleHashDense, Hash] do
- subject = name.new
- puts name, Benchmark.realtime { 500000.times {|n| subject[n] = 4}}.to_s + "   (populate)"
- puts Benchmark.realtime { subject.each{|k, v| }}.to_s + " (each)", ''
+  GC.start
+   subject = name.new
+
+   puts name, Benchmark.realtime { num.times {|n| subject[n] = 4}}.to_s + "   (populate)"
+   puts Benchmark.realtime { subject.each{|k, v| }}.to_s + " (each)"
+   puts Benchmark.realtime { num.times {|n| subject[n]}}.to_s + " (lookup)"
 
 end
