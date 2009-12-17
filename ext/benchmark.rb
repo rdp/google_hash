a00b81481e9d3faa44517a5a6174e3cbc911bde0
rdp <rogerpack2005@gmail.com>
2009-12-17 14:41:19 -0700

    update benchs

--- ext/benchmark.rb
+++ ext/benchmark.rb
@@ -1,6 +1,9 @@
 require './google_hash'
 require 'benchmark'
-
+require 'hitimes'
+def measure
+ Hitimes::Interval.measure { yield }
+end
 
 def go num
   puts
@@ -9,9 +12,9 @@ def go num
   for name in [GoogleHashSparse, GoogleHashDense, Hash] do
     GC.start
     subject = name.new
-    puts name, Benchmark.realtime { num.times {|n| subject[n] = 4}}.to_s + "   (populate)"
-    puts Benchmark.realtime { subject.each{|k, v| }}.to_s + " (each)"
-    puts Benchmark.realtime { num.times {|n| subject[n]}}.to_s + " (lookup)"
+    puts name, measure { num.times {|n| subject[n] = 4}}.to_s + "   (populate)"
+    puts measure { subject.each{|k, v| }}.to_s + " (each)"
+    puts measure { num.times {|n| subject[n]}}.to_s + " (lookup)"
 
   end
 end
--- ext/scale.rb
+++ ext/scale.rb
@@ -1,5 +1,5 @@
 require 'sane'
 require_rel 'benchmark'
-for n in [100, 1000, 100000, 1000000, 1000000, 100000000, 500000000] do
+for n in [ 100000, 1000000, 10000000, 150000000] do
  go n
 end
