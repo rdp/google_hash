d4b41be0f66523ee036707d9bbfc663338a59129
rogerdpack <roger.pack@familysearch.org>
2014-05-28 10:33:05 -0600

    bench gc phew

--- spec/bench_gc.rb
+++ spec/bench_gc.rb
@@ -1,21 +1,22 @@
-puts 'to run different types, modify source code'
-
 require 'sane'
 require_relative '../ext/google_hash'
 require 'benchmark'
-sparse = false
-dense = false
-ruby = true
-if sparse
- a = GoogleHashSparseIntToInt.new
- p 'sparse'
-elsif dense
- p 'dense'
- a = GoogleHashDenseIntToInt.new
-else
- p 'ruby'
- a = []
+if !ARGV[0].in? ['sparse', 'dense', 'ruby']
+  puts 'syntax: bench_gc.rb sparse|dense|ruby'
+  exit 1
 end
-took = Benchmark.realtime { 20_000_000.times {|i| a[i] = i} }
 
-p 'took second', took.group_digits, 'mem used:', OS.rss_bytes.group_digits, 'gc now takes', Benchmark.realtime {GC.start}.group_digits, ObjectSpace.count_objects
+p ARGV[0]
+if ARGV[0] == 'sparse'
+ a = GoogleHashSparseIntToInt.new 
+elsif ARGV[0] == 'dense'
+ a = GoogleHashDenseIntToInt.new
+elsif ARGV[0] == 'ruby'
+ a = Hash.new
+else
+  throw 'never get here'
+end
+
+took = Benchmark.realtime { 2_000_000.times {|i| a[i] = i} }
+
+p 'took second', took.group_digits, 'mem used:', OS.rss_bytes.group_digits, 'each gc now takes', Benchmark.realtime {GC.start}.group_digits, ObjectSpace.count_objects
